5b539634687cddb217fc2313e9225603ee6dfda5
mehuljdaxini <jmehul.daxini@gmail.com>
2021-01-22 15:21:45 +0530

    Create index.py

---
+++ aws_sagemaker/index.py
@@ -0,0 +1,18 @@
+import random
+import string
+from datetime import datetime
+
+def handlerPreprocessing(event,context):
+	letters = string.ascii_lowercase
+	suffix = ''.join(random.choice(letters) for i in range(10))
+	jobParameters = {
+		'name': 'model-trainining-'+str(datetime.date(datetime.now()))+'-'+suffix,
+		'hyperparameters': {
+			'num_of_epochs': '4'
+		}
+	}
+	return jobParameters
+
+def handlerPostprocessing(event,context):
+	print(event)
+	return event
